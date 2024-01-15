ad7882514c1b7639b8ef2fc3ab3c2ddd9f094362
dagadeta <treriwin@gmail.com>
2024-01-15 17:31:27 +0100

    Program finished

--- code.js
+++ code.js
@@ -1,6 +1,39 @@
-function main(chosen) {
-    console.log(chosen);
+function main(playeranswer) {
     let computeranswer = random();
+    document.getElementById("playeranswermain").style.display = "block";
+    document.getElementById("computeranswermain").style.display = "block";
+    document.getElementById("playeranswer").innerHTML = playeranswer;
+    document.getElementById("computeranswer").innerHTML = computeranswer;
+
+    let won;
+    if (playeranswer === "Schere") {
+        if (computeranswer === "Schere") {
+            won = "Niemand";
+        } else if (computeranswer === "Stein") {
+            won = "Computer";
+        } else if (computeranswer === "Papier") {
+            won = "Spieler"
+        }
+    } else if (playeranswer === "Stein") {
+        if (computeranswer === "Schere") {
+            won = "Spieler";
+        } else if (computeranswer === "Stein") {
+            won = "Niemand";
+        } else if (computeranswer === "Papier") {
+            won = "Computer"
+        }
+    } else if (playeranswer === "Papier") {
+        if (computeranswer === "Schere") {
+            won = "Computer"
+        } else if (computeranswer === "Stein") {
+            won = "Spieler"
+        } else if (computeranswer ==="Papier") {
+            won = "Niemand"
+        }
+    }
+
+    document.getElementById("won").style.display = "block";
+    document.getElementById("whowon").innerHTML = won;
 }
 
 function random() {
@@ -13,6 +46,5 @@ function random() {
     } else if (randomnumber === 2) {
         computeranswer = "Papier";
     }
-    console.log(computeranswer);
     return(computeranswer);
 }
